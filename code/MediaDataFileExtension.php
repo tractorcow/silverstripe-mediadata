a638106f2c84467814dfcd0d29529d9db009b154
Damian Mooyman <trionova@yahoo.com>
2012-03-13 19:41:43 -0700

    ADDED: Shortcuts so that video width/height can be accessed in the same way as image dimensions ($File.Width or $File->getWidth()). This allows you to retrieve dimensions of a file indifferent to whether it's an image or a video.

--- code/MediaDataFileExtension.php
+++ code/MediaDataFileExtension.php
@@ -16,4 +16,25 @@ class MediaDataFileExtension extends DataObjectDecorator
     {
         return MediaDataManager::factory()->DataFor($this->owner->FullPath);
     }
+    
+    
+	/**
+	 * Shortcut for retrieving the width of this media for non-image elements
+	 * @return integer|null Width of this media element, or null if not applicable
+	 */
+	function getWidth() {
+		$media = $this->owner->MediaData();
+        if($media && $video = $media->Video())
+            return $video->getWidth();
+	}
+	
+	/**
+	 * Shortcut for retrieving the height of this media for non-height elements
+	 * @return integer|null Height of this media element, or null if not applicable
+	 */
+	function getHeight() {
+		$media = $this->owner->MediaData();
+        if($media && $video = $media->Video())
+            return $video->getHeight();
+	}
 }
